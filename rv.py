267a0a7fc917449e2f1377a66c9d933eb51287d1
Andrey Maykov <maykeye@github.com>
2020-01-05 11:36:40 +0600

    added ability to select a proof

--- rv.py
+++ rv.py
@@ -1,5 +1,5 @@
 from nmigen import Module, Signal, ClockSignal, ClockDomain
-from nmigen.cli import main_parser, main_runner, main as nmigen_main
+from nmigen.cli import main_parser, main_runner
 
 from core import Core
 from instructions.op_imm import OpImmInstr
@@ -18,17 +18,30 @@ def main():
     core.aux_ports.append(clock.clk)
     core.aux_ports.append(clock.rst)
     
+    parser = main_parser()
+    parser.add_argument("--proof", type=str, help="generate signle proof")
+    args=parser.parse_args()
+    required_proof = args.proof
+    proof_generated=False
+
 
     instr = core.add_instruction(OpImmInstr())
-    for proof_class in instr.proofs():
-        p = proof_class()
-        p.run(m, core)
+    for proof_class in instr.proofs():        
+        
+        proof_name = proof_class.__name__.lower()
+        if proof_name.startswith("proof"):
+            proof_name=proof_name[len("proof"):]            
+        if required_proof is None or proof_name == args.proof:
+            proof_class().run(m, core)
+            proof_generated=True
+    if required_proof is not None and not proof_generated:
+        raise Exception(f"Unknown proof {required_proof}")
 
     #core.simulate(m, clock, OpImmInstr.simulate())
 
     if "generate" in sys.argv:
-        nmigen_main(m, ports=core.ports())
+        main_runner(parser, args, m, ports=core.ports())
 
 
 if __name__ == "__main__":
-    main()
+    main()    
