52130955f9597edc891ebd98a0423749fc50d294
Andrey Maykov <maykeye@github.com>
2020-01-06 21:15:52 +0600

    JAL: cleanup
    Removed redundant check for rd=0, inlined return_address

--- instructions/jal.py
+++ instructions/jal.py
@@ -14,12 +14,9 @@ class JalInstr(Instruction):
         return core.jtype.opcode == Opcode.Jal
 
     def implement(self):
-        m : Module = self.core.current_module
         core : Core = self.core
         
-        with m.If(core.jtype.rd != 0):
-            return_address = (core.r.pc + 4)[:core.xlen]
-            core.assign_gpr(core.jtype.rd, return_address)
+        core.assign_gpr(core.jtype.rd, core.r.pc + 4)
         core.move_pc_to_next_instr(core.jtype.imm+4)
         core.emit_debug_opcode(DebugOpcode.JAL, core.jtype.imm)
 
